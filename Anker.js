d3803fc889db31d0ff6ad8e3399956573bbe843d
L-Mo <78338200+L-M0@users.noreply.github.com>
2021-02-20 09:53:19 +0700

    Anker.js

--- Anker.js
+++ Anker.js
@@ -249,10 +249,6 @@ async function starts() {
 		        const nani = await getBuffer(`https://file102.iijj.nl/?file=M3R4SUNiN3JsOHJ6WWQ3aTdPRFA4NW1rRVJIOHV2WXFuOUkxekJrdlNyeER2OFl1MmU2cExjNEJJN2RjaTR5ckhkdHI1VC9NZVkzT1lpbVF1WTB5VjNHRXNvUWJ0QytjdHR3R0FJd2pjQStxbHVlM2szOTFoZ3ErTTVQQUVMWlJhbmh3cWt4Y3hDbUs4L0Rhc3h6MnRFS3VzWG5XZENrWWdob0tETHZkNGN3TzBtekVNdDdBbFpVZHZTK041NDVBMS9YQTVWZW5rT1ZtNzRVbENBSm5aYzFPejhnPQ%3D%3D`, {method: 'get'});
                         client.sendMessage(from, nani, MessageType.audio, {quoted: mek, mimetype: 'audio/mp4', ptt:true})
 		        }
-                        if (auto.match('p')){
-		        const nani = await getBuffer(`https://x2convert.com/id/Thankyou?token=U2FsdGVkX19YdpmFWPQsSKJq7hsno6mQvTs5uyJxixT3gHWbjOzIYbMxPdzocGJ6zXQrrwHZp0vWRhh8pFBn%2bFD8b3m3ClWnoRVpFwppunlNxfjynZFrBuqYRq5GiFXudX7F9kLskwZZfNHCV7e5ay0h5x2dsXXaNsZwrQQ69TBlbnJT0h9D8UAzloxsMY4yTBJb%2bD3rep%2bdqO%2baTYs8jg%3d%3d&s=youtube&id=&h=1702744780907040484`, {method: 'get'});
-                        client.sendMessage(from, nani, MessageType.audio, {quoted: mek, mimetype: 'audio/mp4', ptt:true})
-		        }
                         if (auto.match('fuck')){
 		        const nani = await getBuffer(`https://x2convert.com/id/Thankyou?token=U2FsdGVkX19YdpmFWPQsSKJq7hsno6mQvTs5uyJxixT3gHWbjOzIYbMxPdzocGJ6zXQrrwHZp0vWRhh8pFBn%2bFD8b3m3ClWnoRVpFwppunlNxfjynZFrBuqYRq5GiFXudX7F9kLskwZZfNHCV7e5ay0h5x2dsXXaNsZwrQQ69TBlbnJT0h9D8UAzloxsMY4yTBJb%2bD3rep%2bdqO%2baTYs8jg%3d%3d&s=youtube&id=&h=1702744780907040484`, {method: 'get'});
                         client.sendMessage(from, nani, MessageType.audio, {quoted: mek, mimetype: 'audio/mp4', ptt:true})
