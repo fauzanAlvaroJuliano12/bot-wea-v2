a6135a7c1c10abcd1c134e4c041dab3f7699d46a
L-Mo <78338200+L-M0@users.noreply.github.com>
2021-02-22 22:04:14 +0700

    Add files via upload

---
+++ src/ownermenu.js
@@ -0,0 +1,14 @@
+const ownermenu = (prefix) => { 
+	return `
+	
+╔<[ *OWNER MENU* ]>
+║
+╠➤ ${prefix}setppbot
+╠➤ ${prefix}setnamebot
+╠➤ ${prefix}block
+╠➤ ${prefix}unblock
+╠➤ ${prefix}ban
+╚➤ ${prefix}unban
+`
+}
+exports.ownermenu = ownermenu
